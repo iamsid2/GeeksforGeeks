8b013da48766d6db232ff4b6f747ebd30dece304
iamsid2 <shaktimund97@gmail.com>
2018-07-21 13:08:49 +0530

    Bubble_Sort in py

---
+++ Sorting Algorithms/Bubble Sort/bubble_sort.py
@@ -0,0 +1,14 @@
+def Bubble_Sort(arr,n):
+	for i in range(n-1):
+		for j in range(n-i-1):
+			if arr[j] > arr[j+1]:
+				arr[j],arr[j+1] = arr[j+1], arr[j]
+	print("The array after Bubble_Sort becomes")
+	for i in range(n):
+		print(arr[i], end=' ')
+
+print("Enter the array elements")
+arr = input().split(' ')
+arr = [int(i) for i in arr]
+n = len(arr)
+Bubble_Sort(arr, n)
